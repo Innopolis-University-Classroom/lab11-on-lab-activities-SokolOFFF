1be2a046ec4c72de89bd5c59207b4657f6df1cc6
SokolOFFF <egor1sokolov@gmail.com>
2022-04-04 14:01:22 +0300

    2nd task) q ! git status

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,38 @@
 // your solution here
 //fix all the errors including package errors
+
+public interface LightningPort {
+
+    void chargeByLightning();
+
+}
+
+class AndroidAdapter extends LightningPort{
+    Android android;
+    AndroidAdapter(Android android){
+        this.android = android;
+    }
+
+    @Override
+    public void chargeByLightning() {
+
+    }
+}
+
+public class ChargerStation {
+
+    void chargeByLightning(LightningPort lightningPort){
+        lightningPort.chargeByLightning();
+    }
+}
+
+public class problem2{
+    public static void main(String[] args) {
+        ChargerStation station = new ChargerStation();
+        iPhone iPhone = new iPhone();
+        station.chargeByLightning(iPhone);
+        // focus here
+        Android android = new Android();
+        station.chargeByLightning(AndroidAdapter(android));
+    }
+}
